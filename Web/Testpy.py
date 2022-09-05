967b2825e6d6ca15cbb2979ea11fe6dfa24c86dd
Tobias <tobiasc2004@hotmail.com>
2022-09-05 12:57:22 +0200

    python made

---
+++ Web/Testpy.py
@@ -0,0 +1,18 @@
+# idk wtf im doing
+
+print("Sup bitch")
+
+print("What is your name")
+myName = input()
+print("Its nice to meet you, " + myName)
+
+print("The length of your name is:")
+print(len(myName))
+
+print("How old are you?")
+
+myAge = input()
+
+print("So youll turn Tobs" + str(int(myAge) + 1) + " in a year")
+
+print("That's young as hell")
