803ef0145722ab325a1d3014d04d282c47b29cfe
Tobias <tobiasc2004@hotmail.com>
2022-09-05 14:19:26 +0200

    if else with password

---
+++ Web/if-else.py
@@ -0,0 +1,10 @@
+name = "Tobias"
+
+password = "krisgamer34"
+
+if name == "Tobias" :
+    print("Davs Toby")
+    if password == "krisgamer34" :
+        print("Access granted")
+    else :
+        print("Wrong password")
