6a13bcf34e947296ae571a52b57e3f35b5076241
Tobias <tobiasc2004@hotmail.com>
2022-09-12 14:13:15 +0200

    fixed py

--- Web/Kurs-omregner.py
+++ Web/Kurs-omregner.py
@@ -3,6 +3,6 @@ print("Hvor mange DKK vil du væksle til dollars?")
 penge = input() # spørg om mængde af penge
 
 if int(penge) <= 5 :
-    print("No can do, det er for lidt")
+    print("Så får du " + str(int(penge) / 7.44 - 0.5) + " dollar tilbage efter kommision")
 else :
     print("så får du " + str(int(penge) / 7.44 * 0.98) + " dollar tilbage efter kommision")
